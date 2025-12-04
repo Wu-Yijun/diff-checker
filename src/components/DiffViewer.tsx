7db20b744ee7e80193df28930e5f506e6bfc57e5
Wu-Yijun <wuyijun21@mails.ucas.ac.cn>
2025-12-05 00:20:55 +0800

    fix name display

--- src/components/DiffViewer.tsx
+++ src/components/DiffViewer.tsx
@@ -511,15 +511,15 @@ export const DiffViewer: React.FC<DiffViewerProps> = ({ leftSnippet, rightSnippe
           >
             <svg className="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               {isEditMode ? (
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+              ) : (
                 <>
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                 </>
-              ) : (
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
               )}
             </svg>
-            <span>{isEditMode ? 'View' : 'Edit'}</span>
+            <span>{isEditMode ? 'Edit' : 'View'}</span>
           </button>
 
           {/* Split By Line Toggle */}
